35c8bb8fc7e5572429c5160fde8b1caafb5894c6
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2023-10-22 18:06:28 -0400

    initial setup for mobile foundation

--- src/components/molecules/Navigation/Navigation.tsx
+++ src/components/molecules/Navigation/Navigation.tsx
@@ -5,33 +5,52 @@ import { motion } from "framer-motion";
 
 interface NavigationProps {
     isOpen: boolean;
+    toggleMenu: Function;
 }
 
-function Navigation({ isOpen }: NavigationProps) {
+function Navigation({ isOpen, toggleMenu }: NavigationProps) {
     return (
         <motion.div
-            className={`navbar-links ${isOpen ? "open" : ""}`}
+            className={`nav-container ${isOpen ? "active" : ""}`}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
-            <ul className="desktop-nav">
-                <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
-                    <Link to="/" text="Home" />
-                </motion.li>
-
-                <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
-                    <Link to="/about" text="About" />
-                </motion.li>
-                <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
-                    <Link to="/services" text="Services" /></motion.li>
-                <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
-                    <Link to="/contact" text="Contact" />
-                </motion.li>
-            </ul>
+            <nav>
+            <ul className="mobile-nav">
+                <li className="flex items-center justify-between w-full">
+                <div className="items-center text-white flex justify-center animate__animated animate__backInRight">
+                       <p>Imperfect and Company</p>
+                    </div>                    
+                    <div className="menu-icon-container animate__animated animate__slideInDown animate__slow" onClick={() => toggleMenu()} >
+                        <div className="menu-icon">
+                            <span className="line-1"></span>
+                            <span className="line-2"></span>
+                        </div>
+                    </div>
+                </li>
+            </ul>             
+                <ul className="desktop-nav">
+                    <motion.li whileTap={{ scale: 0.9 }} >                     
+                    </motion.li>
+                    <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
+                        <Link to="/" text="Home" />
+                    </motion.li>
+                    <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
+                        <Link to="/about" text="About" />
+                    </motion.li>
+                    <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
+                        <Link to="/services" text="Services" />
+                    </motion.li>
+                    <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} >
+                        <Link to="/contact" text="Contact" />
+                    </motion.li>
+                </ul>
+            </nav>
         </motion.div>
     );
 }
+
 
 
 export default Navigation;
