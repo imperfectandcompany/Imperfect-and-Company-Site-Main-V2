d115648e9375a32fd90cbebcb9be8d247194b431
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2023-11-08 02:22:09 -0500

    current state. navbar mobile needs to be fixed. asap.

--- src/components/molecules/Navigation/Navigation.tsx
+++ src/components/molecules/Navigation/Navigation.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense } from "react";
 import "./Navigation.css"; // Import the CSS for styling
-import Link from "../../atoms/Link/Link"; // Import the Link component
 import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
 import useNavigationData from "components/hooks/useNavigationData";
@@ -24,10 +23,97 @@ const SkeletonLoader = () => (
     </div>
 );
 
+
 function Navigation({ isOpen, isScrolled, isMenuOpen, toggleMenu }: NavigationProps) {
     const location = useLocation();
     const currentPage = location.pathname;
     const sections = useNavigationData();
+
+    const renderLink = (link: any) => (
+        <motion.li
+            key={link.name}
+            whileHover={{ scale: 0.99 }}
+            whileTap={{ scale: 1.0 }}
+            className={`${currentPage === link.path ? "active" : ""}`}
+        >
+    <a href={link.path} className="link">
+
+                {link.name}
+        </a>
+
+        </motion.li>
+    );
+
+
+    // Consider a function that renders categories for the desktop version
+    const renderDesktopMenu = () => (
+        <>
+            <motion.li className="items-center hidden text-white select-none cursor-pointer justify-center animate__animated animate__backInRight md:block" whileTap={{ scale: 0.9 }} >
+                <a href="/" className='flex space-x-6 ml-6'>
+                    <div className="logo">
+                        <Suspense fallback={<SkeletonLoader />}>
+                            <LogoComponent />
+                        </Suspense>
+                    </div>
+                    <div>
+                        Imperfect and Company
+                    </div>
+                </a>
+            </motion.li>
+            {sections.map((section) => (
+                <div key={section.title} className="dropdown">
+                    <button className="dropbtn">{section.title}</button>
+                    <div className="dropdown-content">
+                        {section.links.map(renderLink)}
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+
+    const renderMobileMenu = () => (
+        <>
+            <motion.li className="items-center hidden text-white select-none cursor-pointer justify-center animate__animated animate__backInRight md:block" whileTap={{ scale: 0.9 }} >
+                <a href="/" className='flex space-x-6 ml-6'>
+                    <div className="logo">
+                        <Suspense fallback={<SkeletonLoader />}>
+                            <LogoComponent />
+                        </Suspense>
+                    </div>
+                    <div>
+                        Imperfect and Company
+                    </div>
+                </a>
+            </motion.li>
+            {sections.map((section) => (
+                <>
+                        {section.links.map(link => (
+                        <>
+                        {renderLink(link)}
+                        </>
+                    ))}
+                </>
+            ))}
+        </>
+    );
+
+    // Adjust this function for the mobile version as needed
+    const renderMobileCategories = () => (
+        <ul className="mobile-nav">
+            <li className="flex items-center justify-between w-full">
+                <div className="items-center text-white flex justify-center animate__animated animate__backInRight">
+                    <p><a href="/">Imperfect and Company</a></p>
+                </div>
+                <div className="menu-icon-container items-center animate__animated animate__slideInDown animate__slow" onClick={() => toggleMenu()} >
+                    <div className="menu-icon">
+                        <span className="line-1"></span>
+                        <span className="line-2"></span>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        // Similar logic but structured for a full-screen overlay or a sidebar
+    );
 
 
     return (
@@ -39,46 +125,15 @@ function Navigation({ isOpen, isScrolled, isMenuOpen, toggleMenu }: NavigationPr
         >
             <nav>
 
+                {/* Render mobile or desktop nav based on screen width */}
                 <ul className="mobile-nav">
-                    <li className="flex items-center justify-between w-full">
-                        <div className="items-center text-white flex justify-center animate__animated animate__backInRight">
-                            <p><a href="/">Imperfect and Company</a></p>
-                        </div>
-
-                        <div className="menu-icon-container items-center animate__animated animate__slideInDown animate__slow" onClick={() => toggleMenu()} >
-                            <div className="menu-icon">
-                                <span className="line-1"></span>
-                                <span className="line-2"></span>
-                            </div>
-                        </div>
-                    </li>
+                    {renderMobileCategories()}
                 </ul>
-                <ul className="desktop-nav flex items-center">
-                    <motion.li className="items-center hidden text-white select-none cursor-pointer justify-center animate__animated animate__backInRight md:block" whileTap={{ scale: 0.9 }} >
-                        <a href="/" className='flex space-x-6 ml-6'>
-                        <div className="logo">
-                                <Suspense fallback={<SkeletonLoader />}>
-                                    <LogoComponent />
-                                </Suspense>
-                            </div> 
-                            <div>
-                                Imperfect and Company
-                            </div>
-                        </a>
-                    </motion.li>
-
-                    {sections.map(section => (
-                        section.links.map(link => (
-                            <motion.li
-                                key={link.name}
-                                whileHover={{ scale: 0.99 }}
-                                whileTap={{ scale: 1.0 }}
-                                className={`${currentPage === link.path ? "active" : ""}`}
-                            >
-                                <Link to={link.path} text={link.name} />
-                            </motion.li>
-                        ))
-                    ))}
+                <ul className="desktop-nav items-center hidden md:flex">
+                   {renderDesktopMenu()}
+                </ul>
+                <ul className="desktop-nav items-center md:hidden">
+                   {renderMobileMenu()}
                 </ul>
             </nav>
         </motion.div>
