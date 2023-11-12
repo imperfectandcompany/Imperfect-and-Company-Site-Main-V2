bc60a1da7cf47c64af17f23e75793f0f5246e428
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2023-11-12 17:14:52 -0500

    added animated text. shortened the section

--- src/components/templates/Postogon/Splash.tsx
+++ src/components/templates/Postogon/Splash.tsx
@@ -1,9 +1,15 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
+import { motion, AnimatePresence } from 'framer-motion';
 import styles from './Splash.module.css';
+
+const words = ['authenticity', 'personalized', 'privacy', 'community', 'creativity', 'inclusive', 'integrated'];
+const swapDuration = 2; // Duration of how long each word is displayed in seconds
 
 const Splash: React.FC = () => {
     const circleRefs = useRef<(HTMLSpanElement | null)[]>([]);
+    const [currentWord, setCurrentWord] = useState('complicated');
+    const [index, setIndex] = useState(0);    
 
     useLayoutEffect(() => {
 
@@ -25,21 +31,49 @@ const Splash: React.FC = () => {
             ease: "power3.inOut",
           });
 
+          const intervalId = setInterval(() => {
+            setIndex((i: number) => (i + 1) % words.length);
+        }, swapDuration * 1000);
+
           return () => {
             circleTl.kill(); // Use circleTl.kill() instead of gsap.killTweensOf()
+            clearInterval(intervalId);
           };
-          
     }, []);
+
+    useEffect(() => {
+        setCurrentWord(words[index] || '');
+    }, [index]);
+
 
 
     return (
         <section className={styles.splash}>
-            <h1>The web is <span className={styles.spanSecondary}>complicated</span>.</h1>
+<h1>
+                The web is{' '}
+                <AnimatePresence mode="wait">
+                    <motion.span
+                        key={currentWord}
+                        initial={{ y: -20, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{ y: 20, opacity: 0 }}
+                        transition={{ duration: 0.5 }}
+                        className={styles.spanSecondary}
+                    >
+                        {currentWord}
+                    </motion.span>
+                </AnimatePresence>
+                .
+            </h1>
             <div className={styles.splashBg}>
                 {Array(8).fill(null).map((_, index) => (
-                    <span key={index} className={styles.circle} ref={el => {
-                        if (el) circleRefs.current[index] = el;
-                    }} />
+                    <span
+                        key={index}
+                        className={styles.circle}
+                        ref={(el) => {
+                            if (el) circleRefs.current[index] = el;
+                        }}
+                    />
                 ))}
             </div>
         </section>
