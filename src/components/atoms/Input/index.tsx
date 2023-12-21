b4b722179a8fd8fe47d3d79b11c3f73b49d9a566
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2023-12-21 04:42:26 +0300

    setup good ui/ux for radiobutton

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -2,54 +2,63 @@ import React, { InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
 import styles from './Input.module.css';
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
-    as?: 'input';
-    className?: string;
-    error?: boolean;
-    warning?: boolean;
-  };
+  as?: 'input';
+  className?: string;
+  error?: boolean;
+  warning?: boolean;
+};
 
 type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
-    as: 'textarea';
-    className?: string;
-    error?: boolean;
-    warning?: boolean;
-  };
+  as: 'textarea';
+  className?: string;
+  error?: boolean;
+  warning?: boolean;
+};
 
-  type RadioProps = InputHTMLAttributes<HTMLInputElement> & {
-    as: 'radio';
-    className?: string;
-    error?: boolean;
-    warning?: boolean;
-    label?: string;
-  };
-  
+type RadioProps = InputHTMLAttributes<HTMLInputElement> & {
+  as: 'radio';
+  className?: string;
+  error?: boolean;
+  warning?: boolean;
+  label?: string;
+};
 
 const InputComponent: React.FC<InputProps> = ({ className, error, warning, ...props }) => {
   const classNames = `${styles.input} ${error ? styles.error : ''} ${warning ? styles.warning : ''} ${className}`;
   return <input className={classNames} {...props} />;
 };
-  
-  const TextInputComponent: React.FC<TextareaProps> = ({ className, error, warning, ...props }) => {
-    const classNames = `${styles.input} ${styles.textarea} ${error ? styles.error : ''} ${warning ? styles.warning : ''} ${className}`;
-    return <textarea className={classNames} {...props} />;
+
+const TextInputComponent: React.FC<TextareaProps> = ({ className, error, warning, ...props }) => {
+  const classNames = `${styles.input} ${styles.textarea} ${error ? styles.error : ''} ${warning ? styles.warning : ''} ${className}`;
+  return <textarea className={classNames} {...props} />;
+};
+
+const RadioComponent: React.FC<RadioProps> = ({ className, error, warning, label, ...props }) => {
+  const [isChecked, setIsChecked] = React.useState(false);
+  const classNames = `${styles.radio} ${error ? styles.error : ''} ${warning ? styles.warning : ''} ${className}`;
+  const labelClassNames = `${styles.radioLabel} ${isChecked ? styles.defaultCursor : ''}`;
+  const id = Math.random().toString(); // generate a unique id
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
   };
-  
-  const RadioComponent: React.FC<RadioProps> = ({ className, error, warning, label, ...props }) => {
-    const classNames = `${styles.radio} ${error ? styles.error : ''} ${warning ? styles.warning : ''} ${className}`;
-    return (
-      <label>
-        <input type="radio" className={classNames} {...props} />
-        {label}
-      </label>
-    );
-  };
+
+  return (
+    <div className={styles.radioContainer}>
+      <div className="flex">
+        <input type="radio" id={id} className={classNames} onChange={handleInputChange} {...props} />
+        <label htmlFor={id} className={labelClassNames}>{label}</label>
+      </div>
+    </div>
+  );
+};
 
 type Props = InputProps | TextareaProps | RadioProps;
 
 const Input: React.FC<Props> = (props) => {
-    if (props.as === 'textarea') {
-      return <TextInputComponent {...props} />;
-    }
+  if (props.as === 'textarea') {
+    return <TextInputComponent {...props} />;
+  }
 
   if (props.as === 'radio') {
     return <RadioComponent {...props} />;
